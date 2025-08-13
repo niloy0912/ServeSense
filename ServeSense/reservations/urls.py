48289f538d9c9f76c5731f388dd1b252b793f726
niloy0912 <mohaimenul12@gmail.com>
2025-08-13 22:45:14 +0600

    Final version before merge

--- ServeSense/reservations/urls.py
+++ ServeSense/reservations/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('create/', views.create_reservation, name='add_reservation'),
     path('edit/<int:reservation_id>/', views.edit_reservation, name='edit_reservation'),
     path('delete/<int:reservation_id>/', views.delete_reservation, name='delete_reservation'),
-     path('accept/<int:reservation_id>/', views.accept_reservation, name='accept_reservation'),
+    path('accept/<int:reservation_id>/', views.accept_reservation, name='accept_reservation'),
     path('', views.home, name='home'),  # Home page view
 ]
